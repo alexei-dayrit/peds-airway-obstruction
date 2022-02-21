d4092aa93f1f2606f729ba4936dd6c048efad83f
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-20 19:17:14 -0800

    More formatting changes.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var $primaryNav = document.querySelector('.primary-navigation');
 var $navToggle = document.querySelector('.mobile-nav-toggle');
 var $cprNavLink = document.querySelector('.cpr-nav-link');
-// var $cprSection = document.querySelector('#cpr-protocol');
-// var topPos = $cprSection.offsetTop;
+var $airwayNavLink = document.querySelector('.airway-nav-link');
+var $surveyNavLink = document.querySelector('.survey-nav-link');
+var $infoNavLink = document.querySelector('.info-nav-link');
+
 
 $navToggle.addEventListener('click', () => {
   var $visibility = $primaryNav.getAttribute('data-visible');
@@ -17,6 +19,16 @@ $navToggle.addEventListener('click', () => {
 });
 
 $cprNavLink.addEventListener('click', () => {
+  $primaryNav.setAttribute('data-visible', false);
+  $navToggle.setAttribute('aria-expanded', false);
+});
+
+$surveyNavLink.addEventListener('click', () => {
+  $primaryNav.setAttribute('data-visible', false);
+  $navToggle.setAttribute('aria-expanded', false);
+});
+
+$infoNavLink.addEventListener('click', () => {
   $primaryNav.setAttribute('data-visible', false);
   $navToggle.setAttribute('aria-expanded', false);
 });
