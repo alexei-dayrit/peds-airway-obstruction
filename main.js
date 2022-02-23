b6e7c1e9ce5443aee4b02c864bcc82278c1b8782
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-23 01:48:37 -0800

    Styled other pages to match home-page and ensured they all link to each other.

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ var $airwayNavLink = document.querySelector('.airway-nav-link');
 var $surveyNavLink = document.querySelector('.survey-nav-link');
 var $infoNavLink = document.querySelector('.info-nav-link');
 var $headerTitle = document.querySelector('.header-title');
+var $mainContainer = document.querySelector('.main-container');
+var $aboutUsBtn = document.querySelector('#about-us');
+var $secondContainer = document.querySelector('.second-container');
 
 $headerTitle.addEventListener('click', function goHome() {
   window.scrollTo(0,0);
@@ -43,9 +46,6 @@ $infoNavLink.addEventListener('click', function (event) {
   window.scrollTo(0, 0);
 });
 
-var $mainContainer = document.querySelector('.main-container');
-var $aboutUsBtn = document.querySelector('#about-us');
-var $secondContainer = document.querySelector('.second-container');
 
 function hideMain (event) {
   $mainContainer.setAttribute('class', 'main-container hidden');
@@ -53,4 +53,10 @@ function hideMain (event) {
   window.scrollTo(0, 0);
 }
 
-$aboutUsBtn.addEventListener('click', hideMain);
+// function showAboutUs (event) {
+//   $mainContainer.setAttribute('class', 'main-container hidden');
+//   $secondContainer.setAttribute('class', 'second-container text-white');
+//   window.scrollTo(0, 0);
+// }
+
+// $aboutUsBtn.addEventListener('click', showAboutUs);
