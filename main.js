dae648584c631910cc6933b6bb6a13163891f07e
Alexei Dayrit <alexeigdayrit@gmail.com>
2022-02-28 00:05:42 -0800

    Added carousel for cpr section.

--- main.js
+++ main.js
@@ -1,62 +1,105 @@
-// var $primaryNav = document.querySelector('.primary-navigation');
-// var $navToggle = document.querySelector('.mobile-nav-toggle');
-// var $cprNavLink = document.querySelector('.cpr-nav-link');
-// var $airwayNavLink = document.querySelector('.airway-nav-link');
-// var $surveyNavLink = document.querySelector('.survey-nav-link');
-// var $infoNavLink = document.querySelector('.info-nav-link');
 var $headerTitle = document.querySelector('.header-title');
 var $mainContainer = document.querySelector('.main-container');
-// var $aboutUsBtn = document.querySelector('#about-us');
-// var $secondContainer = document.querySelector('.second-container');
+var $allImages = document.querySelectorAll('.carousel-item');
+var $prevImage = document.querySelector('.prev-item');
+var $nextImage = document.querySelector('.next-item');
+var $totalImages = $allImages.length;
+var $dotContainer = document.querySelector('.dot-container');
+var $allDots = document.querySelectorAll('.dot');
+var $allTitles = document.querySelectorAll('.image-title');
+var currentImage = 0;
 
 $headerTitle.addEventListener('click', function goHome() {
-  window.scrollTo(0,0);
+  window.scrollTo(0, 0);
 });
 
-// $navToggle.addEventListener('click', function () {
-//   var $visibility = $primaryNav.getAttribute('data-visible');
+function changeCurrentImage() {
+  for (var i = 0; i < $allImages.length; i++) {
+    $allImages[i].setAttribute('class', 'hidden carousel-item protocol-image');
+  }
+  $allImages[currentImage].setAttribute('class', 'visible carousel-item protocol-image');
+  clearInterval(intervalId);
+  intervalId = setInterval(rotateImages, 4000);
+}
 
-//   if ($visibility === "false") {
-//     $primaryNav.setAttribute('data-visible', true);
-//     $navToggle.setAttribute('aria-expanded', true);
-//   } else if ($visibility === "true") {
-//     $primaryNav.setAttribute('data-visible', false);
-//     $navToggle.setAttribute('aria-expanded', false);
-//   }
-// });
+function cycleTitle() {
+  for (var i = 0; i < $allTitles.length; i++) {
+    $allTitles[i].setAttribute('class', 'hidden image-title');
+  }
+  $allTitles[currentImage].setAttribute('class', 'visible image-title')
+}
 
-// function toggleMobileNav (event) {
-//   $primaryNav.setAttribute('data-visible', false);
-//   $navToggle.setAttribute('aria-expanded', false);
-//   $mainContainer.setAttribute('class', 'main-container');
-//   $secondContainer.setAttribute('class', 'second-container text-white hidden');
-// };
+function changeDots(event) {
+  if (event.target.matches('.dot')) {
+    for (var i = 0; i < $allDots.length; i++) {
+      if ($allDots[i] === event.target) {
+        $allDots[i].className = 'dot fas fa-circle';
+        for (var a = 0; a < $allImages.length; a++) {
+          $allImages[a].setAttribute('class', 'hidden carousel-item protocol-image');
+        }
+        $allImages[i].setAttribute('class', 'visible carousel-item protocol-image');
+        currentImage = i;
+      } else {
+        $allDots[i].className = 'dot far fa-circle';
+      }
+    }
+  }
+  cycleTitle();
+  clearInterval(intervalId);
+  intervalId = setInterval(rotateImages, 4000);
+}
 
-// $cprNavLink.addEventListener('click', toggleMobileNav);
-// $airwayNavLink.addEventListener('click', toggleMobileNav);
-// $surveyNavLink.addEventListener('click', toggleMobileNav);
+$dotContainer.addEventListener('click', changeDots);
 
-// $infoNavLink.addEventListener('click', function (event) {
-//   $primaryNav.setAttribute('data-visible', false);
-//   $navToggle.setAttribute('aria-expanded', false);
-//   $mainContainer.setAttribute('class', 'main-container hidden');
-//   $secondContainer.setAttribute('class', 'second-container text-white');
-//   window.scrollTo(0, 0);
-// });
+function goToPrevImage() {
+  if (currentImage === 0) {
+    currentImage = $totalImages - 1;
+  } else {
+    currentImage--;
+  }
+  cycleDots();
+  cycleTitle();
+  changeCurrentImage();
+  clearInterval(intervalId);
+  intervalId = setInterval(rotateImages, 4000);
+}
 
+function goToNextImage() {
+  if (currentImage === $totalImages - 1) {
+    currentImage = 0;
+  } else {
+    currentImage++;
+  }
+  cycleDots();
+  cycleTitle();
+  changeCurrentImage();
+  clearInterval(intervalId);
+  intervalId = setInterval(rotateImages, 4000);
+}
 
-// function hideMain (event) {
-//   $mainContainer.setAttribute('class', 'main-container hidden');
-//   $secondContainer.setAttribute('class', 'second-container text-white');
-//   window.scrollTo(0, 0);
-// }
+$prevImage.addEventListener('click', goToPrevImage);
+$nextImage.addEventListener('click', goToNextImage);
 
+function cycleDots() {
+  for (var i = 0; i < $allDots.length; i++) {
+    $allDots[i].setAttribute('class', 'dot far fa-circle');
+  }
+  $allDots[currentImage].setAttribute('class', 'dot fas fa-circle');
+  clearInterval(intervalId);
+  intervalId = setInterval(rotateImages, 4000);
+}
 
-// SECOND NAV
+function rotateImages() {
+  currentImage++;
+  if (currentImage < 5) {
+    changeCurrentImage();
+    cycleDots();
+    cycleTitle();
+  } else {
+    currentImage = -1;
+    clearInterval(intervalId);
+    intervalId = setInterval(rotateImages, 4000);
+  }
+}
 
-// var $toggleButton = document.querySelector('.toggle-button');
-// var $navBarLinks = document.querySelector('.navbar-links');
-
-// $toggleButton.addEventListener('click', function() {
-//   $navBarLinks.classList.toggle('active');
-// });
+var intervalId = setInterval(rotateImages, 4000);
